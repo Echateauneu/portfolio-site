23b46d6db524a5665582cbbfe9ac0d916c1b54c9
Emmanuel Chateauneu <emmanuel.chateauneu@gmail.com>
2025-11-18 03:41:17 +0100

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
+import Home from "./pages/Home.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
