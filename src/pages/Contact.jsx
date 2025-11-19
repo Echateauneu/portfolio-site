f3eed0c536ed161ad558da4de70669b17c7bd882
Emmanuel Chateauneu <emmanuel.chateauneu@gmail.com>
2025-11-19 07:46:46 +0100

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,8 @@
+export default function Contact() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Contact</h1>
+      <p>Email: emmanuel.chateauneu@gmail.com</p>
+    </div>
+  );
+}
